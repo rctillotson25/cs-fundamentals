2ddf9a4a156801030ec7cf8424b15c63ae788f34
tilly <rctillotson25@gmail.com>
2015-02-02 08:50:39 -0600

    Added functionality for rotating an nxn matrix clockwise. CCW is next! :)

--- questions/Main.java
+++ questions/Main.java
@@ -3,9 +3,17 @@ package questions;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Matrix m = new Matrix(4, 4);
+	public static int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
 
+	public static void main(String[] args) {
+		Matrix m = new Matrix(matrix);
 		m.print();		
+		
+		System.out.println("Rotating Clockwise.");
+		m.rotateCW();
+		m.print();
+
+		System.out.println("Rotating Counterclockwise.");
+		m.rotateCCW();
 	}
 }
--- questions/Matrix.java
+++ questions/Matrix.java
@@ -1,17 +1,50 @@
 package questions;
 
+import java.util.Stack;
+
 public class Matrix {
 	private int[][] matrix;
+	private int n;
 
-	public Matrix(int i, int j) {
-		matrix = new int[i][j];
+	public Matrix(int n) {
+		matrix = new int[n][n];
+		this.n = n;
+	}
+
+	public Matrix(int[][] matrix) {
+		this.n = matrix.length;
+		this.matrix = matrix;
+	}
+
+	public int[][] getMatrix() { 
+		return matrix;
+	}
+
+	// O(n) - each element is visited once. However, this requires O(n) AUXILARY space. 
+	public void rotateCW() {
+		int[][] pivot = new int[n][n];
+		
+		// rotate cw means item in (i, j) gets moved to (j, n - 1 - i)
+		for (int row = 0; row < n; row++) {
+			for (int col = 0; col < n; col++) {
+				pivot[col][n-1-row] = matrix[row][col];
+			}
+		}		
+		
+		matrix = pivot;	
+	}
+
+	public void rotateCCW() {
+		System.out.println("Rotated Counter Clockwise.");
 	}
 
 	public void print() {
-		for (int i = 0; i < matrix[i].length; i++) {
-			for (int j = 0; i < matrix[j].length; j++) {
-				System.out.print(matrix[i][j] + " ")
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
 			}
+			System.out.println();
 		}
 	}
+
 }
