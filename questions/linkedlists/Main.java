c6a8ed1f574e4ca347cc61208d36a4ed342f568f
tilly <rctillotson25@gmail.com>
2015-02-07 16:56:41 -0600

    Created new folder / class for linked list exercises.

---
+++ questions/linkedlists/Main.java
@@ -0,0 +1,26 @@
+/*
+ * This class is a quick place to answer linked list questions in CTCI
+ * 
+ * Authored on 2/7/2015 by rctillotson25
+ *
+ */
+
+package linkedlists;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Hashtable;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Linked List exercises.");
+	}
+
+
+	public static void removeDuplicates(List<String> lst) {
+	
+		
+	}
+
+}
