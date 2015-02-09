2a61312a7c0d8517c28e1ac48ed953d033c240a6
Ryan <rctillotson25@gmail.com>
2015-02-09 15:33:58 -0600

    Added function that will binary insert a number into another called insertMN in the bit classes.

--- bits/Practice.java
+++ bits/Practice.java
@@ -17,6 +17,12 @@ public class Practice {
 		System.out.println("2,1 " + getBit(2,1));
 		System.out.println("256,8 " + getBit(256,8));
 
+		// getBitValue tests
+		System.out.println("Value 1,0 " + getBitValue(1,0));
+		System.out.println("Value 1,1 " + getBitValue(1,1));
+		System.out.println("Value 2,1 " + getBitValue(2,1));
+		System.out.println("Value 256,8 " + getBitValue(256,8));
+		
 		// clearBit tests
 		System.out.println("Clear 2,1 " + clearBit(2,1));
 		System.out.println("Clear 3,1 " + clearBit(3,1));
@@ -29,10 +35,17 @@ public class Practice {
 		// updateBit tests
 		System.out.println("Set 0,8,ON " + Integer.toBinaryString(updateBit(0,8,ON)));
 		System.out.println("Set 1,0,OFF " + Integer.toBinaryString(updateBit(1,0,OFF)));
+
+		// insertMN tests
+		System.out.println("insertMN 128,7,0,3 " + insertMN(128,7,0,3) + " = 135");
 	}
 
 	public static boolean getBit(int num, int i) {
 		return ((num & (1 << i)) != 0);
+	}
+
+	public static int getBitValue(int num, int i) {
+		return (num >> i) & 1; 
 	}
 
 	public static int clearBit(int num, int i) {
@@ -48,5 +61,12 @@ public class Practice {
 		int mask = ~(1 << i);
 		return (num & mask) | (v << i);	
 	}	
+
+	public static int insertMN(int n, int m, int i, int j)  {
+		for (int k = 0; k < j-i; k++) {
+			n = updateBit(n, k+i, getBitValue(m, k));
+		}
+		return n;
+	}
 
 }
