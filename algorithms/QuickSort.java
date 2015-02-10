12a55f2b37b2a6c33a277933af6164a185ff240d
Ryan <rctillotson25@gmail.com>
2015-02-10 12:44:08 -0600

    Added initial class for quicksort implementation.

---
+++ algorithms/QuickSort.java
@@ -0,0 +1,16 @@
+/*
+ * Implementation of Quick Sort to sort
+ * an array in place.
+ *
+ * Authored on 2/10/2015 by rctillotson25
+ *
+ */
+package algorithms;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		System.out.println("This is quicksort.");
+	}
+
+}
