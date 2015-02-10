b775b82f36ded844bf1e906e838d6154c4664f71
Ryan <rctillotson25@gmail.com>
2015-02-10 08:30:40 -0600

    Initialized the HeapSort class and got it ready for implementation.

---
+++ algorithms/HeapSort.java
@@ -0,0 +1,25 @@
+/*
+ * Basic implementation of the heap sort algorithm.
+ *
+ * Heap sort is good because it has a running time of
+ * O(n lg n), but it also sorts only in place and takes O(1) auxilary space.
+ * Authored on 2/10/2015 by rctillotson25
+ *
+ * 
+ */
+
+package algorithms;
+
+public class HeapSort {
+
+	public static void main(String[] args) {
+		System.out.println("This class will be used to run the heap sort algorithm.");
+	}
+
+	public static int[] heapSort(int[] a) {
+		System.out.println("Heap sort.");
+		return new int[1];
+	}
+
+
+}
