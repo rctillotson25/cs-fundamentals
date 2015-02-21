a62ecd154690b19bcc461ec1b9e3e2253978be05
tilly <rctillotson25@gmail.com>
2015-02-21 09:21:22 -0600

    Continued implementation of MinHeap in Java.

--- trees/heaps/MinHeap.java
+++ trees/heaps/MinHeap.java
@@ -1,7 +1,7 @@
 /*
  * This class will hold a minimum heap.
  *
- *
+ * 
  */
 
 package heaps;
@@ -9,8 +9,16 @@ package heaps;
 
 public class MinHeap implements Heap {
 
+	private int[] heap;
+	private int size;
+	private int maxSize;
+
 	@Override
 	public void heapify(int i) {
 		return;
+	}
+
+	public void buildMinHeap() {
+
 	}	
 }
