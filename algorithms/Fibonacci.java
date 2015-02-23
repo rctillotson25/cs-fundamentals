4dc9f8b08b31d4b56631021722a9138f281ae81f
Ryan <rctillotson25@gmail.com>
2015-02-23 13:08:10 -0600

    Initial fibonacci sequence program - going to use this to practice dynamic algorithms.

---
+++ algorithms/Fibonacci.java
@@ -0,0 +1,30 @@
+/**
+ * This class will be used to implement an algorithm that
+ * implements the Fibonacci sequence several different ways.
+ *
+ * Authored on 2/23/2015 by rctillotson25
+ */
+
+package algorithms;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		System.out.println("Fib 3  = 3 - " + fibonacciBrute(3));
+		System.out.println("Fib 5  = 8 - " + fibonacciBrute(5));
+		System.out.println("Fib 35 = 9227465 - " + fibonacciBrute(35));
+	}
+
+	// Classic, recursive method for computing fibonacci numbers. 
+	// Wasteful and slow. O(1.6^n) Exponential time.
+	private static int fibonacciBrute(int n) {
+
+		if (n == 0) {
+		        return 0;
+		} else if (n == 1) {
+			return 1;
+		} else {
+			return fibonacciBrute(n-1) + fibonacciBrute(n-2);
+		}
+	}
+}
