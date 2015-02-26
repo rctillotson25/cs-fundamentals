4f35ace3e0e7be52187bc41a5d5bf749364fd0f5
Ryan <rctillotson25@gmail.com>
2015-02-26 11:29:45 -0600

    Added dynamic programming solution for Fibonacci sequence calculation.

--- algorithms/Fibonacci.java
+++ algorithms/Fibonacci.java
@@ -15,6 +15,8 @@ public class Fibonacci {
 		System.out.println("Fib 35 = 9227465 - " + fibonacciBrute(35));
 
 		System.out.println("Fib 40 = " + fibonacciBrute(40));
+
+		System.out.println("FibDyn 35 = 9227465  - " + fibonacciDynamic(35));
 	}
 
 	// Classic, recursive method for computing fibonacci numbers. 
@@ -28,5 +30,26 @@ public class Fibonacci {
 		} else {
 			return fibonacciBrute(n-1) + fibonacciBrute(n-2);
 		}
+	}
+
+
+	// Dynamic programming method for handling the fibonacci sequence.
+	// Optimized dynamic algorithm - O(n) time and O(n) total space. Woot!
+	private static int fibonacciDynamic(int n) {
+
+		// a(n-2)
+		int a2 = 0;
+		// a(n-1)
+		int a1 = 1;
+	
+		// x stores the value originally in a(n-2)
+		// set a(n-2) = a(n-1) & a(n-1) = x {a(n-2)} + a(n-1)	
+		for (int i = 2; i <= n; i++) {
+			int x = a2;
+			a2 = a1;
+			a1 = a1 + x;
+		}
+		// a1 is always pointing to the most recently calculated fibonacci number
+		return a1;
 	}
 }
